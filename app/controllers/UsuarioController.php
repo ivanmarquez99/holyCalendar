301d25f78222b7857e4e374821426bc79561cc99
Iván Márquez <93972423+ivanmarquez99@users.noreply.github.com>
2023-04-15 13:37:24 +0200

    Formulario de registro y arreglo de rutas

--- app/controllers/UsuarioController.php
+++ app/controllers/UsuarioController.php
@@ -6,10 +6,19 @@ class UsuarioController extends Controller{
 		$f3->set('tituloPagina','holyCalendar');
         $f3->set('header','header.htm');
         $f3->set('footer','footer.htm');
-        $template=new Template;
-        echo $template->render('Usuario/login.htm');
+        echo \Template::instance()->render('../templates/layout/header.htm');
+        echo View::instance()->render('Usuario/login.htm');
+        echo \Template::instance()->render('../templates/layout/footer.htm');
+	}
+
+  function register($f3){
+		$f3->set('tituloPagina','Registro');
+        $f3->set('header','header.htm');
+        $f3->set('footer','footer.htm');
+        echo \Template::instance()->render('../templates/layout/header.htm');
+        echo View::instance()->render('Usuario/register.htm');
+        echo \Template::instance()->render('../templates/layout/footer.htm');
 	}
 
 
 }
-?>
---
+++ ui/js/script.js
@@ -0,0 +1,4 @@
+register = document.querySelector('#register')
+register.addEventListener("click", function() {
+  window.location.href = "register"
+});
