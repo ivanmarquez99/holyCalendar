5cc8ce3cd2469a01b5a2d73086b29074d87d808d
ivanmarquez99 <ivanaurelio.s258702@cesurformacion.com>
2023-05-15 22:14:46 +0200

    Integracion TinyMCE y copia de fecha de inicio

--- ui/js/agenda.js
+++ ui/js/agenda.js
@@ -77,6 +77,14 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     calendar.render();
 
+    tinymce.init({
+        selector: 'textarea',
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name'
+    });
+
     var elementos = ListNextEvent.getElementsByTagName('li');
 
     var arregloElementos = Array.from(elementos);
@@ -113,3 +121,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
+
+function copyValue(valor) {
+
+    document.querySelector("#fecha_fin").value = valor;
+};
--- ui/js/script.js
+++ ui/js/script.js
@@ -14,6 +14,17 @@ window.addEventListener("load", function() {
     })
 })
 
+window.addEventListener("load", function() {
+
+    tinymce.init({
+        selector: 'textarea',
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name'
+    });
+})
+
 
 function verificarPasswords() {
  
@@ -48,4 +59,16 @@ function verificarPasswords() {
         return true;
     }
 
+}
+
+
+function descriptionModal(id) {
+
+   var description = document.querySelector("#"+id).getAttribute('data-description');
+   document.querySelector("#eventDescription").innerHTML = description;
+
+   let myModal = new bootstrap.Modal('#modalDescription', {
+    keyboard: false
+    });
+    myModal.show();
 }
